dbed058a9bf2572a8f879987f545c08cf80ab3e0
Dirty Industry <xiao1_yu3@163.com>
2018-07-10 15:59:25 +0800

    change electron-packager to electron-builder

--- src/main/index.js
+++ src/main/index.js
@@ -1,6 +1,7 @@
 'use strict'
 
 import { app, BrowserWindow } from 'electron'
+import pkg from '../../package.json'
 
 /**
  * Set `__static` path to static files in production
@@ -8,6 +9,10 @@ import { app, BrowserWindow } from 'electron'
  */
 if (process.env.NODE_ENV !== 'development') {
   global.__static = require('path').join(__dirname, '/static').replace(/\\/g, '\\\\')
+}
+
+if (process.platform === 'win32') {
+  app.setAppUserModelId(pkg.build.appId)
 }
 
 let mainWindow
@@ -45,3 +50,23 @@ app.on('activate', () => {
     createWindow()
   }
 })
+
+/**
+ * Auto Updater
+ *
+ * Uncomment the following code below and install `electron-updater` to
+ * support auto updating. Code Signing with a valid certificate is required.
+ * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-electron-builder.html#auto-updating
+ */
+
+/*
+import { autoUpdater } from 'electron-updater'
+
+autoUpdater.on('update-downloaded', () => {
+  autoUpdater.quitAndInstall()
+})
+
+app.on('ready', () => {
+  if (process.env.NODE_ENV === 'production') autoUpdater.checkForUpdates()
+})
+ */
--- src/renderer/components/MainForm/MainListItem.vue
+++ src/renderer/components/MainForm/MainListItem.vue
@@ -1,176 +1,137 @@
 <template>
-  <div  class="card" v-bind:class="{'selected': isSelected}" @click.exact="$emit('selectItem', spider)" 
-    @click.ctrl.exact="$emit('ctrlSelectItem', spider)" @click.shift.exact="$emit('shiftSelectItem', spider)">
-    <div class="outerdiv">
-      <div class="headdiv">
-        <div class="checkdiv">
-          <el-switch v-model="isActive"></el-switch>
-          <div>{{isActiveDesc}}</div>
-        </div>
-        <div class="infos">
-          <p>{{spider.EntryName}}</p>
-          <p>{{spider.FirstUrl}}</p>
-          <p>{{spider.Repeat}}</p>
-          <p>{{spider.Time}}</p>
-          <p>{{lastExecute.Time}} {{lastExecute.Status}}</p>
-        </div>
-      </div>
-      <div class="buttondiv">
-        <el-button-group>
-          <el-button plain size="mini" icon="el-icon-arrow-right" type="success" @click.exact.stop="runSpider"></el-button>
-          <el-button plain size="mini" icon="el-icon-edit" type="info" @click.exact.stop="editSpider"></el-button>
-          <el-popover placement="top" width="300" v-model="logVisible">
-            <div class="logpop">
-            <p>{{spider.EntryName}} 历史纪录</p>
-            <ul v-if="logs.length">
-              <li v-for="(item, index) in logs" :key='index'>
-                <div>{{item.Time}} {{item.Status}}</div>
-              </li>
-            </ul>
-            <p v-else>
-              Nothing left in the log.
-            </p>
-            </div>
-            <el-button plain size="mini" icon="el-icon-document" type="info" slot="reference"></el-button>
-          </el-popover>
-          <el-popover placement="top" width="160" v-model="deleteConfirmVisible">
-            <p>确定删除{{spider.EntryName}}吗？</p>
-            <div style="text-align: right; margin: 0">
-              <el-button size="mini" type="text" @click="deleteConfirmVisible = false">取消</el-button>
-              <el-button type="primary" size="mini" @click="removeSpider">确定</el-button>
-            </div>
-            <el-button plain size="mini" icon="el-icon-delete" type="danger" slot="reference"></el-button>
-          </el-popover>
-        </el-button-group>
-      </div>
+<div class="card" v-bind:class="{'selected': spider.isSelected}" @click.exact="$emit('selectItem', spider)" @click.ctrl.exact="$emit('ctrlSelectItem', spider)" @click.shift.exact="$emit('shiftSelectItem', spider)">
+  <div class="headdiv">
+    <div class="checkdiv">
+      <el-switch v-model="isActive" @click.stop></el-switch>
+      <div>{{isActiveDesc}}</div>
+    </div>
+    <div class="infos">
+      <p>{{spider.EntryName}}</p>
+      <p>{{spider.FirstUrl}}</p>
+      <p>{{spider.Repeat}}</p>
+      <p>{{spider.Time}}</p>
+      <p>{{lastExecute.Time}} {{lastExecute.Status}}</p>
     </div>
   </div>
+  <div class="buttondiv">
+    <el-button-group>
+      <el-button plain size="mini" icon="el-icon-arrow-right" type="success" @click.exact.stop="runSpider"></el-button>
+      <el-button plain size="mini" icon="el-icon-edit" type="info" @click.exact.stop="editSpider"></el-button>
+      <el-popover placement="top" width="300" v-model="logVisible">
+        <div class="logpop">
+          <p>{{spider.EntryName}} 历史纪录</p>
+          <ul v-if="logs.length">
+            <li v-for="(item, index) in logs" :key="index">
+              <div>{{item.Time}} {{item.Status}}</div>
+            </li>
+          </ul>
+          <p v-else>Nothing left in the log.</p>
+        </div>
+        <el-button plain size="mini" icon="el-icon-document" type="info" slot="reference"></el-button>
+      </el-popover>
+      <el-popover placement="top" width="160" v-model="deleteConfirmVisible">
+        <p>确定删除{{spider.EntryName}}吗？</p>
+        <div style="text-align: right; margin: 0">
+          <el-button size="mini" type="text" @click="deleteConfirmVisible = false">取消</el-button>
+          <el-button size="mini" type="primary" @click="removeSpider">确定</el-button>
+        </div>
+        <el-button plain size="mini" icon="el-icon-delete" type="danger" slot="reference"></el-button>
+      </el-popover>
+    </el-button-group>
+  </div>
+</div>
 </template>
 
 <script>
-  export default {
-    props: {
-      spider: {
-        type: Object,
-        required: true
-      }
+export default {
+  props: {
+    spider: {
+      type: Object,
+      required: true
+    }
+  },
+  data () {
+    return {
+      deleteConfirmVisible: false,
+      logVisible: false
+    }
+  },
+  computed: {
+    logs: function () {
+      let that = this
+      return this.$store.state.SpiderList.loglist.filter(function (item) {
+        return item.Name === that.spider.EntryName
+      })
     },
-    data () {
-      return {
-        deleteConfirmVisible: false,
-        logVisible: false
-      }
-    },
-    computed: {
-      logs: function () {
-        let that = this
-        return this.$store.state.SpiderList.loglist.filter(function (item) {
-          return item.Name === that.spider.EntryName
+    lastExecute: function () {
+      if (this.logs.length === 0) {
+        return ''
+      } else {
+        this.logs.sort(function (x, y) {
+          return ((x.Time === y.Time) ? 0: ((x.Time > y.Time) ? 1 : -1))
         })
-      },
-      lastExecute: function () {
-        if (this.logs.length === 0) {
-          return ''
-        } else {
-          this.logs.sort(this.SortByTime)
-          return this.logs[this.logs.length - 1]
-        }
-      },
-      isSelected: function () {
-        return this.spider.isSelected
-      },
-      isActive: {
-        get: function () {
-          return this.spider.Active
-        },
-        set (value) {
-          if (this.$store.state.SpiderList.socket === null) {
-            this.$store.dispatch('initSocket')
-          }
-          let socket = this.$store.state.SpiderList.socket
-          let setmessage = { Head: 'SetActive', Body: [this.spider.EntryName, value] }
-          socket.send(JSON.stringify(setmessage))
-        }
-      },
-      isActiveDesc: function () {
-        if (this.isActive) {
-          return '启用'
-        } else {
-          return '禁用'
-        }
+        return this.logs[this.logs.length - 1]
       }
     },
-    methods: {
-      runSpider (event) {
-        if (this.$store.state.SpiderList.socket === null) {
-          this.$store.dispatch('initSocket')
-        }
-        let socket = this.$store.state.SpiderList.socket
-        let message = { Head: 'Run', Body: [JSON.stringify(this.spider)] }
-        socket.send(JSON.stringify(message))
-        this.$message('执行' + this.spider.EntryName)
-        // this.stopBubble(event)
+    isActive: {
+      get: function () {
+        return this.spider.Active
       },
-      removeSpider (event) {
-        this.deleteConfirmVisible = false
-        if (this.$store.state.SpiderList.socket === null) {
-          this.$store.dispatch('initSocket')
-        }
-        let socket = this.$store.state.SpiderList.socket
-        let setmessage = { Head: 'RemoveSpider', Body: [JSON.stringify(this.spider)] }
-        socket.send(JSON.stringify(setmessage))
-        this.$message.error('删除' + this.spider.EntryName)
-        // this.stopBubble(event)
-      },
-      editSpider (event) {
-        // console.log(this.spider)
-        this.$store.commit('setSpiderToEdit', this.spider)
-        this.$router.push({ path: '/edit' })
-        // this.stopBubble(event)
-      },
-      SortByTime (x, y) {
-        return ((x.Time === y.Time) ? 0 : ((x.Time > y.Time) ? 1 : -1))
-      },
-      stopBubble (event) {
-        if (event & event.stopPropagation) {
-          event.stopPropagation()
-        } else {
-          window.evnet.cancelBubble = true
-        }
+      set (value) {
+        let setmessage = {Head: 'SetActive', Body: [this.spider.EntryName, value]}
+        this.$store.state.SpiderList.socket.send(JSON.stringify(setmessage))
+      }
+    },
+    isActiveDesc () {
+      if (this.isActive ===  true) {
+        return '启用'
+      } else {
+        return '禁用'
       }
     }
+  },
+  methods: {
+    runSpider (event) {
+      let message = {Head: 'Run', Body: [JSON.stringify(this.spider)]}
+      this.$store.state.SpiderList.socket.send(JSON.stringify(message))
+      this.$message('执行' + this.spider.EntryName)
+    },
+    removeSpider (event) {
+      this.deleteConfirmVisible = false
+      let setmessage = {Head: 'RemoveSpider', Body: [JSON.stringify(this.spider)]}
+      this.$store.state.SpiderList.socket.send(JSON.stringify(setmessage))
+      this.$message.error('删除' + this.spider.EntryName)
+    },
+    editSpider (event) {
+      this.$store.commit('setSpiderToEdit', this.spider)
+      this.$router.push({path: '/edit'})
+    }
   }
+}
 </script>
 
-<style>
+<style scoped>
   .card {
-    min-width: 630px;
-    margin-top: 10px;
-    padding: 10px;
-    border-radius: 8px;
-    border: rgb(201, 201, 201) 1px solid;
-  }
-
-  .outerdiv {
+    min-width: 630rem;
+    margin-top: 10rem;
+    padding: 10rem;
+    border-radius: 8rem;
+    border: rgb(200, 200, 200) 1rem solid;
     display: flex;
     flex-wrap: nowrap;
     justify-content: space-between;
     align-items: center;
   }
-
   .logpop {
-    border: rgb(201, 201, 201) 1px solid;
+    border: rgb(200, 200, 200) 1rem solid;
   }
-
   .headdiv {
     display: flex;
     align-items: center;
   }
-
   .checkdiv {
-    width: 50px;
+    width: 50rem;
   }
-
   .infos p {
     margin: 0;
     padding: 0;
@@ -178,20 +139,17 @@
     overflow: hidden;
     text-overflow: ellipsis;
   }
-
   .infos {
-    max-width: 400px;
+    max-width: 400rem;
   }
-
   .buttondiv {
-    min-width: 120px;
+    min-width: 120rem;
   }
-
   .selected {
-    border: rgb(0, 119, 255) 1px solid;
-    box-shadow: rgba(18, 29, 39,0.3) 0 0 10px;
+    border: rgb(0, 120, 255) 1rem solid;
+    box-shadow: rgba(20, 30, 40, 0.3) 0 0 10rem;
   }
-  .card:hover{
-    box-shadow: rgba(18, 29, 39,0.3) 0 0 10px;
+  .card:hover {
+    box-shadow: rgba(20, 30, 40, 0.3) 0 0 10rem;
   }
 </style>
